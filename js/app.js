a00077c86254afc3e08d86fc1e7416d091fc1814
mario <marioachil@gmail.com>
2019-01-31 11:12:58 +0400

    MaJ JS & Responsive

--- js/app.js
+++ js/app.js
@@ -6,52 +6,68 @@ $(document).ready(()=>{
   const lientUtilesBtn = $('#lientUtilesBtn');
   const contactBtn = $('#contactBtn');
 
-  /**section to display */
-  let accueilSec = $('#accueil'); 
- let quiSommeNousSec = $('#qui-nousSommes');
+  
+ /**section to display */
+ let firstCard = $('#firstCard'); 
+ let mission  = $('#mission');
  let nosTravauxSec =  $('#nos-travaux');
  let lientUtilesSec =  $('#lient-utiles');
  let contactSec = $('#contact');
-
-  /**Function to display sections */
-  homeBtn.on('click',()=>{   
-      accueilSec.fadeIn('slow');
-      accueilSec.css({"margin-top": "259px"})
-      quiSommeNousSec.fadeOut();
-      nosTravauxSec.fadeOut();
-      lientUtilesSec.fadeOut();    
-  });
-  quiSommeNousBtn.on('click',()=>{  
-    quiSommeNousSec.fadeIn('slow');
-    accueilSec.fadeOut();
-    nosTravauxSec.fadeOut();
-    lientUtilesSec.fadeOut();   
-});
-  nosTravauxBtn.on('click',()=>{
-    nosTravauxSec.fadeIn('slow');
-    quiSommeNousSec.fadeOut();
-    accueilSec.fadeOut();   
-    lientUtilesSec.fadeOut(); 
-
-});
-  lientUtilesBtn.on('click',()=>{
-    lientUtilesSec.fadeIn('slow');
-    nosTravauxSec.fadeOut();
-    quiSommeNousSec.fadeOut();
-    accueilSec.fadeOut();  
-});
  
 window.sr = ScrollReveal();
-sr.reveal('.navbar-brand',{
+sr.reveal('header',{
   duration:500,
   origin:'top',
   distance:'250px',
   easing: 'linear'
-});
-sr.reveal('.slogan',{
+ });
+ sr.reveal('.slogan',{
     duration:500,
     origin:'top', 
     distance:'350px',
     easing: 'linear'
+  });
+ 
+  homeBtn.on('click',()=>{ 
+  sr.reveal(firstCard,{
+    duration:500,
+    origin:'right', 
+    distance:'-350px',
+    easing: 'linear'
+  });
+});
+
+homeBtn.on('click',()=>{ 
+sr.reveal(secondCard,{
+  duration:500,
+  origin:'left', 
+  distance:'-350px',
+  easing: 'linear'
+});
+});
+homeBtn.on('click',()=>{ 
+  sr.reveal(mission,{
+    duration:500,
+    origin:'left', 
+    distance:'-350px',
+    easing: 'linear'
+  });
+  });
+
+  $(function () {
+    // Smooth Scrolling
+    $('a[href*="#"]:not([href="#"])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname ==
+        this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
   });
 });
